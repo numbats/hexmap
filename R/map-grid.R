db998d5a6ff81bf3f95a90e648b967a6710b10e3
Emi <dr.emi.tanaka@gmail.com>
2022-10-03 12:03:05 +1100

    skeleton for new functions

---
+++ R/map-grid.R
@@ -0,0 +1,8 @@
+
+#' Create a hexagonal grid from geospatial polygons
+#'
+#' @param x An `sf` object.
+#' @export
+map_grid <- function(x) {
+  # here compute the grid
+}
---
+++ R/map-match.R
@@ -0,0 +1,14 @@
+
+
+#' Match geospatial polygons
+#'
+#' Given two geospatial polygons `x` and `y`, assign the regions
+#' from `y` to the regions in `x`.
+#'
+#' @param x,y An `sf` object of the same size.
+#' @return An sf object with the geometry from `x` with rows
+#'   matched from `y`.
+#' @export
+map_match <- function(x, y) {
+  # compute
+}
